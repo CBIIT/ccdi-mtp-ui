17c0b5385d772e08f397bff0b654629faa47e3f7
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2023-03-06 15:26:47 -0500

    update configVersion value back to the main branch

--- apps/platform/src/config.js
+++ apps/platform/src/config.js
@@ -1,5 +1,5 @@
 // Configuration Object
-const configVersion = window.injectedEnv.configVersion ?? "CCDIMTP-556";
+const configVersion = window.injectedEnv.configVersion ?? "main";
 
 const config = {
   urlApi:
