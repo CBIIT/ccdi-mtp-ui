415697c522f2584993c5558e5094521b188c2c58
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2023-03-13 16:55:32 -0400

    integrate Graphql API for Epigenetic Modification widget under Target page

--- apps/platform/src/sections/target/EpigeneticModification/Body.jsx
+++ apps/platform/src/sections/target/EpigeneticModification/Body.jsx
@@ -3,27 +3,15 @@ import React from 'react';
 import config from '../../../config';
 import { Body as EpigeneticModificationBody } from '../../common/EpigeneticModification';
 import Description from './Description';
-// import usePlatformApi from '../../../hooks/usePlatformApi';
-// import Summary from './Summary';
+import usePlatformApi from '../../../hooks/usePlatformApi';
+import Summary from './Summary';
 
 import EPIGENETIC_MODIFICATION_QUERY from './EpigeneticModificationQuery.gql';
 
 function Body({ definition, id, label: symbol }) {
-  
-  /*
-  // TODO: Replace w/ the following
   const summaryRequest = usePlatformApi(
     Summary.fragments.targetEpigeneticModificationSummary
   );
-  */
-  const summaryRequest = { 
-    data: {
-      methylationByGene: { count: 1, },
-      methylationByIsoform: { count: 1, },
-    },
-    loading: false,
-    error: null,
-  }
 
   const variables = { ensemblId: id };
   const dataDownloaderFileStem = `EpigeneticModification-${id}`;
--- apps/platform/src/sections/target/EpigeneticModification/index.js
+++ apps/platform/src/sections/target/EpigeneticModification/index.js
@@ -3,18 +3,8 @@ export const definition = {
   name: 'Epigenetic Modification',
   shortName: 'EM',
   hasData: data => {
-    /*
-    TODO: Replace with epigeneticModification related data
     const { methylationByGene, methylationByIsoform, } = data;
-    const hasCount = methylationByGene.count > 0 || methylationByIsoform.count > 0
-    */
-    const { snvByGene, snvByVariant, cnvByGene, fusionByGene, fusion } = data;
-    const hasCount =
-      snvByGene.count > 0 ||
-      snvByVariant.count > 0 ||
-      cnvByGene.count > 0 ||
-      fusionByGene.count > 0 ||
-      fusion.count > 0;
+    const hasCount = methylationByGene?.count > 0 || methylationByIsoform?.count > 0;
     return hasCount;
   },
   color: '#5ca300',
