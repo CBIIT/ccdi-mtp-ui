079a0e1f00e248d2da1f4815b2f665ae13d6bb2e
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2023-04-04 05:21:26 -0400

    add TCGA fun under GX - Evidence page

--- apps/platform/src/sections/evidence/OpenPedCanGeneExpression/Body.jsx
+++ apps/platform/src/sections/evidence/OpenPedCanGeneExpression/Body.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
-import { getGeneDiseaseGtexPlot } from '../../../utils/externalAPI';
+import { getGeneDiseaseGtexPlot, getGeneDiseaseTcgaPlot } from '../../../utils/externalAPI';
 import config from '../../../config';
 import Description from './Description';
 
-import { getData } from './Summary';
+import { getGtexData, getTcgaData } from './Summary';
 import { Body as OpenPedCanGeneExpressionBody } from '../../common/OpenPedCanGeneExpression';
 
 function Body({ definition, id, label }) {
+
   const { ensgId: ensemblId, efoId } = id;
   const downloadFileName = `OpenPedCanGeneExpression-${ensemblId}-${efoId}`;
-  const imageAlt = 'Single-gene single-disease all-GTEx-tissue-subgroups';
+  const gtexImageAlt = 'Single-gene single-disease all-GTEx-tissue-subgroups';
+  const tcgaImageAlt = 'Single-gene all-diseases all-TCGA';
   const configAPI = `${config.mtpConfig}/front-end/page_evidence`;
+
   return (
     <OpenPedCanGeneExpressionBody
       definition={definition}
       id={id}
-      getData={getData}
-      getPlot={getGeneDiseaseGtexPlot}
       label={label}
       entity="evidence"
       Description={Description}
-      fileStem={downloadFileName}
-      imageAlt={imageAlt}
       configAPI={configAPI}
+      fileStem={downloadFileName}
+      getGtexData={getGtexData}
+      getGtexPlot={getGeneDiseaseGtexPlot}
+      gtexImageAlt={gtexImageAlt}
+      getTcgaData={getTcgaData}
+      getTcgaPlot={getGeneDiseaseTcgaPlot}
+      tcgaImageAlt={tcgaImageAlt}
     />
   );
 }
