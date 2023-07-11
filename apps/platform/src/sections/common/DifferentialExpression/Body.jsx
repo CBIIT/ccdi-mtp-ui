8ef2a2bf38bc345b9dca6909fff7a7c3e1d6a63b
Nahomtes <44936471+Nahomtes@users.noreply.github.com>
2023-07-11 12:29:35 -0400

    POC for R Plotly using iframe

--- apps/platform/src/sections/common/DifferentialExpression/Body.jsx
+++ apps/platform/src/sections/common/DifferentialExpression/Body.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Grid } from '@material-ui/core';
-import { Buffer } from 'buffer';
+// import { Buffer } from 'buffer';
 import SectionItem from '../../../components/Section/SectionItem';
 import useColumnConfiguration from '../../../hooks/useColumnConfiguration';
 import DisplayPlot from './Image';
@@ -62,8 +62,9 @@ function Body({
 
       const callback = (resData) => {
         // Convert the plot data to base64-encoded image source
-        const base64 = Buffer.from(resData).toString('base64');
-        const imageSrc = base64;
+        /*const base64 = Buffer.from(resData).toString('base64');
+        const imageSrc = base64;*/
+        const imageSrc = resData;
         // Update the plotData object with the new plot image source
         setPlotData(data => ({ ...data, [selectedOption]: { ...data[selectedOption], [tab]: imageSrc } }));
         setLoading(false);
