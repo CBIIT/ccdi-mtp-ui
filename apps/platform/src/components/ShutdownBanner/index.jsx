c51c976490b631a25a28c6fdbb2258371e94298f
Chen <jonkiky@gmail.com>
2024-06-24 09:51:50 -0400

    add message bar

--- apps/platform/src/components/ShutdownBanner/index.jsx
+++ apps/platform/src/components/ShutdownBanner/index.jsx
@@ -6,15 +6,8 @@ function ShutdownBanner() {
   return (
     <section className="nci-shutdown-banner" aria-label="Government Funding Lapse">
       <div className="nci-shutdown-banner__body">
-        <h2>Government Funding Lapse</h2>
         <p style={{ margin: 0 }}>
-          Because of a lapse in government funding, the information on this website may not be up 
-          to date, transactions submitted via the website may not be processed, and the agency 
-          may not be able to respond to inquiries until appropriations are enacted. 
-          The NIH Clinical Center (the research hospital of NIH) is open. For more details about 
-          its operating status, please visit  <a href="https://cc.nih.gov/">cc.nih.gov</a>. 
-          Updates regarding government operating status and resumption of normal operations can be 
-          found at <a href="https://opm.gov/">OPM.gov</a>.
+          The Molecular Targets Platform (MTP) will be undergoing maintenance Monday, June 24, starting at 3:00 PM Eastern Daylight Time (EDT) through Tuesday, June 25, at approximately 12:00 PM EDT. During this period, MTP may be unavailable or unstable.
         </p>
       </div>
     </section>
